a12780ed28104eb0631d3e4ae67a708925e6b027
ant9712 <ant97_12@mail.ru>
2023-03-12 18:31:07 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace project  {
+
+  class Hello_world  {
+
+   static void Main() {
+
+    Console.Write("Здравствуйте! Введите свое имя: ");
+    string name = Console.ReadLine();
+    Console.WriteLine($"Еще раз здравствуй {name}!");
+
+   }
+
+  }
+
+
+
+}
+
+
